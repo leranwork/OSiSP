f81bf2e948080d281da72fa066d474d61f8676bc
leranwork <61198612+leranwork@users.noreply.github.com>
2020-05-06 10:20:13 +0300

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,6 +1,3 @@
-
-////Первый процесс семафорами передаёт второму текущее время, а второй выводит его в форматированном для человека виде. Внимание: никаких средств взаимодействия(файлов, pipe, ...), кроме семафоров и мьютексов, не использовать!
-
 #include <unistd.h>
 #include <sys/types.h>
 #include <semaphore.h>
@@ -13,31 +10,104 @@
 #define SEM_NAME_2 "/semaphore2"
 int main(){
 
-    int zn =0;
-    sem_t *semaphore2 = sem_open(SEM_NAME_2, O_CREAT, 0777, 0);
-    sem_post(semaphore2);
-    for(int i = 0; i < 5; i++){
-        sem_t *semaphore1 = sem_open(SEM_NAME_1, O_CREAT, 0777, i);
-        sem_getvalue(semaphore1, &zn);
-        sem_wait(semaphore1);
-        printf("Programm1 sends %d\n\n", zn);
-        sem_unlink(SEM_NAME_1);
-        sem_post(semaphore2);
-    }
+    sem_t *semaphore, *mutex;
+    time_t timer = 0;
 
+    mutex=sem_open(SEM_NAME_1,O_CREAT,0777,1);
+    semaphore=sem_open(SEM_NAME_2,O_CREAT,0777,1);
+
+    printf("0\n1\n");
+
+    for(int i=0;i<5;i++){
+        sem_wait(mutex);
+
+        if(sem_getvalue(semaphore, &timer) == -1){
+            printf("ERROR in get value\n");
+        }
+        timer = time(NULL); //вычисляем текущее время
+        printf("Текущее время1: %ld \n", timer);
+        sem_init(semaphore,0,(int)timer);
+
+        sem_post(mutex);
+        sleep(1);
+    }
+    sem_unlink(SEM_NAME_1);
     sem_unlink(SEM_NAME_2);
-    sleep(2);
     return 0;
 }
 
+
+
 //#include <stdio.h>
 //#include <time.h>
-//int main(void)
+//#include <semaphore.h>
+//#include <fcntl.h>
+//#include <unistd.h>
+//#include <string.h>
+//
+//int main(void){
+//    
+//    sem_t *sem1, *sem2, *sem3;
+//    printf("1");
+//    if((sem1 = sem_open("sig5", O_CREAT, 0777, 0)) == SEM_FAILED){
+//        printf("Error in sem_open1");
+//    };
+//    printf("1");
+//
+//    if((sem2 = sem_open("sig6", O_CREAT, 0777, 1)) == SEM_FAILED){
+//        printf("Error in sem_open2");
+//    };
+//    printf("1");
+//    if((sem3 = sem_open("data3",O_CREAT, 0777, 0)) == SEM_FAILED){
+//       printf("Error in sem_open3");
+//    }
+//    
+//    time_t timer;
+//    
+//    for(int i=0; i<10;i++){
+//        sem_wait(sem2);
+//        timer = time(NULL);
+//        printf("Текущее время: %ld",timer);
+//        
+//        for(int i=0;i<(int)timer;i++){
+//            sem_post(sem3);
+//        }
+//        sem_post(sem1);
+//        sleep(1);
+//    }
+//    
+//    sem_unlink("sig5");
+//    sem_unlink("sig6");
+//    sem_unlink("data3");
+//    return 0;
+//}
+
+//#include <semaphore.h>
+//#include <stdio.h>
+//#include <time.h>
+//#include <fcntl.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//int main()
 //{
-//struct tm *ptr;
-//time_t It;
-//It = time(NULL);
-//ptr = localtime(&It);
-//    printf("%s", asctime(ptr));
-//return 0;
+//    struct tm *u;
+//    char *f;
+//    const time_t timer=time(NULL);
+//    u=localtime(&timer);
+//    f=asctime(u);
+//    char semaphore[]="/semaphore";
+//    sem_t *s=sem_open(semaphore,O_CREAT,0777,1);
+//    int file=0;
+//    sem_wait(s);
+//    if((file=open("tß.txt",O_RDWR))==-1)
+//    {
+//        printf("Open failed");
+//        exit(-1);
+//    }
+//    write(file,u,100);
+//    printf("Process 1 write %d\n ",u);
+//    close(file);
+//    sem_post(s);
+//    sem_close(s);
+//    return 0;
 //}
--- 2.c
+++ 2.c
@@ -1,30 +1,91 @@
-////Первый процесс семафорами передаёт второму текущее время, а второй выводит его в форматированном для человека виде. Внимание: никаких средств взаимодействия(файлов, pipe, ...), кроме семафоров и мьютексов, не использовать!
-
-#include <unistd.h>
-#include <sys/types.h>
 #include <semaphore.h>
-#include <stdio.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include <unistd.h>
+#include <string.h>
 #define SEM_NAME_1 "/semaphore1"
 #define SEM_NAME_2 "/semaphore2"
-int main(){
-    int zn;
-    sem_t *semaphore1 = sem_open(SEM_NAME_1, O_CREAT, 0777, 0);
-    sem_t *semaphore2 = sem_open(SEM_NAME_2, O_CREAT, 0777, 0);
-    for(int i = 0; i < 5; i++){
-        sem_wait(semaphore2);
-//        sleep(1);
-        sem_getvalue(semaphore1, &zn);
-//        sem_wait(semaphore2);
-        printf("Programm2 gets simbol: %d\n", zn);
-        printf("\n");
+
+
+int main()
+{
+    sem_t *semaphore, *mutex;
+    time_t timer = 0;
+
+    mutex=sem_open(SEM_NAME_1,O_RDWR,0);
+    semaphore=sem_open(SEM_NAME_2,O_RDWR,1);
+
+    for(int i=0;i<5;i++){
+        sem_wait(mutex);
+        if(sem_getvalue(semaphore, &timer) == -1){
+            printf("Error in get value");
+        }
+        printf("Текущее время2: %s", ctime(&timer));
+        sem_close(semaphore);
+        sem_post(mutex);
         sleep(1);
-        sem_post(semaphore1);
     }
     sem_unlink(SEM_NAME_1);
     sem_unlink(SEM_NAME_2);
     return 0;
+
 }
+
+
+//#include <semaphore.h>
+//#include <fcntl.h>
+//#include <unistd.h>
+//#include <string.h>
+//
+//int main(void){
+//
+//    printf("1");
+//    sem_t *sem1 = sem_open("sig5", O_RDWR,1);
+//    printf("2");
+//    sem_t *sem2 = sem_open("sig6", O_RDWR);
+//    printf("3");
+//    sem_t *sem3 = sem_open("data3", O_RDWR);
+//
+//    for(int i=0;i<10;i++){
+//        sem_wait(sem1);
+//        time_t timer;
+//        sem_getvalue(sem3, &timer);
+//        int count = (int)timer;
+//
+//        while(count!=0){
+//            sem_wait(sem3);
+//            count--;
+//        }
+//
+//        printf("Текущее время2: %s", ctime(&timer));
+//
+//        sem_post(sem2);
+//        sleep(1);
+//    }
+//    return 0;
+//}
+
+//#include <semaphore.h>
+//#include <stdio.h>
+//#include <time.h>
+//#include <fcntl.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//int main()
+//{
+//    char f[25];
+//    char semaphore[]="/semaphore";
+//    sem_t *s=sem_open(semaphore,O_RDWR,0777);
+//    int file=0;
+//    sem_wait(s);
+//    if((file=open("t.txt",O_RDWR))==-1)
+//    {
+//        printf("Open failed");
+//        exit(-1);
+//    }
+//    read(file,f,25);
+//    printf("Process 2 read: %s\n",asctime(f));
+//    close(file);
+//    sem_post(s);
+//    sem_close(s);
+//    return 0;
+//}
